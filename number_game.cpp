9848f549bd4a8ed6befa5346da8f69b95156cf4f
Alex Leibowitz <alexanderbleibowitz@gmail.com>
2019-05-08 12:30:55 -0500

    completed puzzle prompt

--- number_game.cpp
+++ number_game.cpp
@@ -12,14 +12,27 @@ int* getNumbers(int difficulty) {
   return numbers;
 }
 
-void playGameAtDifficulty(int difficulty) {
+void writeWelcome(int difficulty) {
   std::cout << "Welcome to the number game.\n";
   std::cout << "You have selected difficulty level "; 
   std::cout << difficulty << "\n";
-  
-  int* numbers;
-  numbers = getNumbers(difficulty);
-  std::cout << *numbers << endl;
+}
+
+void printPuzzle(int* numbers) {
+  int sum = 0;
+  int product = 1;
+  for (int i = 0; i < 3; i++) {
+    int number = (*numbers + i);
+    sum += number;
+    product *= number;
+  }
+  std::cout << "Find three numbers whose product is " << product << " and whose sum is " << sum << endl;
+}
+
+void playGameAtDifficulty(int difficulty) {
+  writeWelcome(difficulty);
+  int* numbers = getNumbers(difficulty);
+  printPuzzle(numbers);
 }
 
 int main(int argc, char **argv)
