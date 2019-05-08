cb1db0f2239f014fa765c1b2e33505d7377a7887
Alex Leibowitz <alexanderbleibowitz@gmail.com>
2019-05-08 16:20:00 -0500

    mvp

--- number_game.cpp
+++ number_game.cpp
@@ -3,7 +3,8 @@
 
 using namespace std; 
 
-int* getNumbers(int difficulty) {
+int* getNumbers(int difficulty) 
+{
   srand((unsigned)time(0));
   static int numbers[3];
   for (int i = 0; i < 3; i++) {
@@ -12,27 +13,69 @@ int* getNumbers(int difficulty) {
   return numbers;
 }
 
-void writeWelcome(int difficulty) {
-  std::cout << "Welcome to the number game.\n";
-  std::cout << "You have selected difficulty level "; 
-  std::cout << difficulty << "\n";
+void writeWelcome(int difficulty) 
+{
+  std::cout 
+    << "Welcome to the number game." << endl 
+    << "You have selected difficulty level " 
+    << difficulty << endl;
 }
 
-void printPuzzle(int* numbers) {
-  int sum = 0;
-  int product = 1;
+struct puzzle {
+    int sum = 0;
+    int product = 1;
+};
+
+puzzle getResults(int* numbers) {
+  puzzle results;
   for (int i = 0; i < 3; i++) {
-    int number = (*numbers + i);
-    sum += number;
-    product *= number;
+    int number = *(numbers + i);
+    results.sum += number;
+    results.product *= number;
   }
-  std::cout << "Find three numbers whose product is " << product << " and whose sum is " << sum << endl;
+  return results;
+}
+
+puzzle getPuzzle(int difficulty) {
+  int* numbers = getNumbers(difficulty);
+  puzzle p = getResults(numbers);
+  return p;
+}
+
+void printPuzzle(puzzle p) 
+{
+  std::cout 
+    << "Find three integers (>= 0) whose product is " 
+    << p.product 
+    << " and whose sum is " 
+    << p.sum << endl;
+}
+
+int* getAnswer() 
+{
+  static int answers[3];
+  std::cout 
+    << "Enter the three numbers below, separated by spaces." << endl;
+  std::cin 
+    >> answers[0] 
+    >> answers[1] 
+    >> answers[2];
+  return answers;
+}
+
+bool checkAnswers(puzzle p, int* guess) {
+  puzzle g = getResults(guess);
+  return g.product == p.product && g.sum == p.sum;
 }
 
 void playGameAtDifficulty(int difficulty) {
   writeWelcome(difficulty);
-  int* numbers = getNumbers(difficulty);
-  printPuzzle(numbers);
+  puzzle p = getPuzzle(difficulty);
+  printPuzzle(p);
+  int* guess = getAnswer();
+  bool isCorrect = checkAnswers(p, guess);
+  if (isCorrect) cout << "You got it!" << endl;
+  else cout << "Try again, bucko..." << endl;
 }
 
 int main(int argc, char **argv)
