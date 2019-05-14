a9bd56409b6c914161edd0e984dfc696b61bf61d
Alex Leibowitz <alexanderbleibowitz@gmail.com>
2019-05-14 07:05:54 -0500

    wrote explanation of bull cow game

---
+++ bull_cow.cpp
@@ -0,0 +1,4 @@
+/* 
+  In this game, the player is asked to guess an isogram (a word with no letters repeating). If the player gets the right letter in the right place, she gets a "bull"; if she guesses the right letter in the wrong place, she gets a "cow". She has 10 tries to guess the correct word (winning and losing conditions).
+*/
+
